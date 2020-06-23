3d12bd562dd3a000957b315920601ae5fd66d760
unknown <ashesdangol@hotmail.com>
2020-06-23 18:09:45 +1000

    before webpack

--- app/public/wp-content/themes/study-group/functions.php
+++ app/public/wp-content/themes/study-group/functions.php
@@ -1,6 +1,11 @@
 <?php 
     function headScripts(){
-         wp_enqueue_style('headStyle__link', get_stylesheet_uri());
+         wp_enqueue_script('scripts', get_theme_file_uri('/assets/scripts/scripts-bundled.js'),null,'1.0',true);
+         wp_enqueue_style('googlefont__link', '//fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,500;1,700&display=swap');
+         wp_enqueue_style('main-css', get_stylesheet_uri());
+
+        //  wp_enqueue_script('main-js', 'http://localhost:3000/bundled.js',NULL,'1.0',true);
+
     }
     add_action('wp_enqueue_scripts','headScripts');
 
