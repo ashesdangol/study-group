6cc37bfc48274da8333c7ec2ef9e0f3241492412
unknown <ashesdangol@hotmail.com>
2020-06-23 23:58:13 +1000

    lodash npm

--- app/public/wp-content/themes/study-group/functions.php
+++ app/public/wp-content/themes/study-group/functions.php
@@ -1,6 +1,6 @@
 <?php 
     function headScripts(){
-         wp_enqueue_script('scripts', get_theme_file_uri('/assets/scripts/scripts-bundled.js'),null,'1.0',true);
+         wp_enqueue_script('scripts', get_theme_file_uri('/assets/scripts/App.js'),null,'1.0',true);
          wp_enqueue_style('googlefont__link', '//fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,500;1,700&display=swap');
          wp_enqueue_style('main-css', get_stylesheet_uri());
 
